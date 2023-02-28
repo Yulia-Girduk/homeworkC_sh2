20ae30eea1128ce254615187fe6883c567a67823
Nick Gir <mbiko@ya.ru>
2023-02-28 20:16:44 +0300

    add task10

---
+++ task10/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход 
+//трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+// Math.Abs(-5) = 5, возвращает число без знака 
+if ((99 < Math.Abs(number)) && (Math.Abs(number) < 1000)) 
+{
+    int result = Math.Abs((number % 100)/10);
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Введите трехзначное число!!!");
+}
+
