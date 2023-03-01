148216198d7b9d799e4030abfcafccede76a3bd3
Nick Gir <mbiko@ya.ru>
2023-03-01 20:05:07 +0300

    add changes

--- task10/Program.cs
+++ task10/Program.cs
@@ -2,7 +2,7 @@
 //трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 Console.Write("Введите трехзначное число: ");
-int number = int.Parse(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
 
 // Math.Abs(-5) = 5, возвращает число без знака 
 if ((99 < Math.Abs(number)) && (Math.Abs(number) < 1000)) 
